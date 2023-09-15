1db5d9aabaf78438ac87e32f576612754b6fb27a
Phai <104388483+phai-zk@users.noreply.github.com>
2023-09-15 20:44:05 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require('express');
+
+
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(express.json());
+
+// Setup DB
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',false);
+
+mongoose.connect(process.env.mongoURI, {useNewUrlParser: true});
+
+//Setup database models
+require('./model/Quest')
+require('./model/Account');
+require('./model/Item');
+require('./model/Tag');
+
+
+//setup routes
+require('./routes/authenticationRoutes')(app);
+require('./routes/itemRoutes')(app);
+require('./routes/questRoute')(app);
+require('./routes/adminRoute')(app);
+require('./routes/wOofRoute')(app);
+
+const port = 13756
+app.listen(port, () => {
+    console.log("Listening on " + port)
+})
