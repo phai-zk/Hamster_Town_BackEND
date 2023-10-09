db8caa98b088fe394c1b2c4a6437d9f52b0165b0
Phai <chisanupong.m34.1@gmail.com>
2023-10-09 15:26:29 +0700

    sad

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -200,40 +200,4 @@ module.exports = (app) => {
 
   })
 
-
-
-  app.post('/api/accounts/checkLoot', async (req, res) => {
-    try {
-      const accounts = await Account.find();
-
-      for (const account of accounts) {
-        if (account.player) {
-          // ทำตราบเท่านี้สำหรับ account ที่เป็น account เก่าและไม่มี loot
-          // ตราบเท่านี้คือตัวอย่าง คุณสามารถเพิ่มเงื่อนไขของคุณตามต้อง
-          await Account.findOne({ username: rusername }).updateOne(
-            {player: {
-              equip: {
-                Helmet: { name: "", quantity: 0 },
-                Chestplate: { name: "", quantity: 0 },
-                Legging: { name: "", quantity: 0 },
-                Boot: { name: "", quantity: 0 },
-                Hold: { name: "", quantity: 0 },
-              },
-              stat:
-              {
-                lv: 1
-              }
-            }},
-            req.body
-          );
-        }
-      }
-
-      res.status(200).json({ message: 'Check complete' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-
 };
